2841c7c4aa8bb0b6a0eb039ac07f59dbff26b07d
Lorenzo Ricciardiello <lorenzo1995111@gmail.com>
2023-09-10 14:04:59 +0100

    welcomeStyle

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,75 @@
 import './bootstrap';
 import "bootstrap/dist/js/bootstrap.min.js";
+
+
+//! SCROLL NAVBAR
+
+let navbar = document.querySelector("#navbar");
+let links = document.querySelectorAll(".nav-link")
+// console.log(window)
+
+
+window.addEventListener('scroll', ()=>{
+    console.log(window.scrollY)
+    let scrolled = window.scrollY;
+
+
+
+    if(scrolled > 100){
+        navbar.classList.add('navbar-blur' ,'bg-1')
+        links.forEach((link) => {
+            link.style.color = " burlywood";
+            link.addEventListener('mouseenter', ()=>{
+                link.style.color = 'black'
+                link.style.borderBottom = '2px solid black'
+            })
+            link.addEventListener('mouseleave', ()=>{
+                link.style.color = 'burlywood';
+                link.style.borderBottom = "none";
+            })
+
+            });
+    }
+    else{
+        navbar.classList.remove('navbar-blur' ,'bg-1')
+        links.forEach((link) => {
+            link.style.color = "black";
+
+            link.addEventListener('mouseenter', ()=>{
+                link.style.color = 'burlywood'
+                link.style.borderBottom = '2px solid burlywood'
+            })
+            link.addEventListener('mouseleave', ()=>{
+                link.style.color = 'black';
+                link.style.borderBottom = "none";
+            })
+            });
+
+    }
+
+
+});
+
+
+
+//! SCROLL SECTION
+
+
+// elements
+var elements_to_watch = document.querySelectorAll('.watch');
+// callback
+var callback = function(items){
+  items.forEach((item) => {
+    if(item.isIntersecting){
+      item.target.classList.add("in-page");
+    } else{
+      item.target.classList.remove("in-page");
+    }
+  });
+}
+// observer
+var observer = new IntersectionObserver(callback, { threshold: 0.6 } );
+// apply
+elements_to_watch.forEach((element) => {
+  observer.observe(element);
+});
--- resources/views/components/cards.blade.php
+++ resources/views/components/cards.blade.php
@@ -1,25 +1,27 @@
 <section class="container mt-5" >
 <div class="row" >
 <div class="col-12 col-md-3 my-3 bordi">
-<div class="card" style="width: 18rem;height: 28rem;">
-<img class="px-2 py-2" style="height:250px;" src="{{ Storage::url($product->img)}}" alt="">
+<div class="card" style="width: 20rem;height: 30rem;">
+<img class="px-2 py-2" style="height:270px; width:200px;" src="{{ Storage::url($product->img)}}" alt="">
 <div class="card-body text-center">
 <h5 class="card-title">{{$product->name}}</h5>
 <p class="card-text">{{$product->price}}€</p>
 <a href="{{route('product.edit',compact('product'))}}"class="btn">Modifica</a>
 
-<form action="{{route('product.delete', compact('product'))}}" method="POST"> 
+<form action="{{route('product.delete', compact('product'))}}" method="POST">
 
-        @method('DELETE') 
+        @method('DELETE')
 
-        @csrf 
+        @csrf
 
-        <button type="submit" >Cancella</button> 
+        <button type="submit" >Cancella</button>
 
-    </form> 
-    
+    </form>
+
 </div>
 </div>
 </div>
 </div>
 </section>
+
+
